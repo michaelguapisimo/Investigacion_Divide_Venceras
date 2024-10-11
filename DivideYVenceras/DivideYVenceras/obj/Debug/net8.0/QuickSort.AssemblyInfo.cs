7cbb74c689ffcb389b2e4287c585d70b70b88e4b
abojii <abojorgec@gmail.com>
2024-10-10 21:57:41 -0600

    BinarySearch

--- DivideYVenceras/DivideYVenceras/obj/Debug/net8.0/QuickSort.AssemblyInfo.cs
+++ DivideYVenceras/DivideYVenceras/obj/Debug/net8.0/QuickSort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickSort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fcb6105539737c6833e01fc501a9a2a1bb0240")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickSort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickSort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
