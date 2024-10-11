f7fcb6105539737c6833e01fc501a9a2a1bb0240
michaelguapisimo <mramirezc@uamv.edu.ni>
2024-10-10 21:04:55 -0600

    segundo

--- DivideYVenceras/DivideYVenceras/Program.cs
+++ DivideYVenceras/DivideYVenceras/Program.cs
@@ -12,15 +12,16 @@ namespace QuickSort
         {
             int[] datos = { -1, 2, 777, 4, -56, 12, 40, 1, 2, 10, 0, 7 };
             Console.WriteLine("Datos NO ordenados");
-
+            PrintArray(datos);
         }
 
         static void Swap(int[] datos, int i, int j) 
         { 
             int temp = datos[i];
             datos[i] = datos[j];
+            datos[j]= temp;
         }
-
+        
         static void PrintArray(int[] datos)
         {
             foreach (var item in datos) 
