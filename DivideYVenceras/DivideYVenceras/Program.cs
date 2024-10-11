c1d33015b737687ec45bce0cda12b532ffb247ed
michaelguapisimo <mramirezc@uamv.edu.ni>
2024-10-10 20:43:33 -0600

    primero

---
+++ DivideYVenceras/DivideYVenceras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int[] datos = { -1, 2, 777, 4, -56, 12, 40, 1, 2, 10, 0, 7 };
+            Console.WriteLine("Datos NO ordenados");
+
+        }
+
+        static void Swap(int[] datos, int i, int j) 
+        { 
+            int temp = datos[i];
+            datos[i] = datos[j];
+        }
+
+        static void PrintArray(int[] datos)
+        {
+            foreach (var item in datos) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+}
