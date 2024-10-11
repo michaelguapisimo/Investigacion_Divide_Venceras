5c092bf0b6dd69c5caa55115e72b01239a4525f9
abojii <abojorgec@gmail.com>
2024-10-10 22:40:19 -0600

    Diseño de interfaz

--- DivideYVenceras/BinarySearch/Form1.Designer.cs
+++ DivideYVenceras/BinarySearch/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbNumeros = new System.Windows.Forms.ComboBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(3, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(147, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Ingrese un numero:";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(62, 125);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(94, 49);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Buscar";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(529, 71);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // cmbNumeros
+            // 
+            this.cmbNumeros.FormattingEnabled = true;
+            this.cmbNumeros.Location = new System.Drawing.Point(211, 68);
+            this.cmbNumeros.Name = "cmbNumeros";
+            this.cmbNumeros.Size = new System.Drawing.Size(121, 28);
+            this.cmbNumeros.TabIndex = 5;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(238, 123);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(94, 51);
+            this.btnOrdenar.TabIndex = 6;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.FormattingEnabled = true;
+            this.lbResultado.ItemHeight = 20;
+            this.lbResultado.Location = new System.Drawing.Point(37, 226);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(318, 104);
+            this.lbResultado.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Location = new System.Drawing.Point(37, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(318, 158);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.cmbNumeros);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbNumeros;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.ListBox lbResultado;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
